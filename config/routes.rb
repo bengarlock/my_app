9cbf29abf879f43ff1a809366c7facf42dc08ae4
Ben Garlock <ben.garlock@gmail.com>
2021-02-20 16:57:44 -0500

    testing...

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root 'members#index'
+
   resources :members
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,4 +23,12 @@ ActiveRecord::Schema.define(version: 2021_02_20_175044) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "ssn"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
