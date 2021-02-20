257c50bbe29ddf2d1a12e5406c12a2b1e8cda956
Ben Garlock <ben.garlock@gmail.com>
2021-02-20 21:13:13 +0000

    server to client commit

---
+++ releases/20210220193914/db/migrate/20210220175044_create_members.rb
@@ -0,0 +1,11 @@
+class CreateMembers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :members do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :member_number
+
+      t.timestamps
+    end
+  end
+end
