fa6bb05482fce4c3faa43c40703bb110323be051
Ben Garlock <ben.garlock@gmail.com>
2021-02-21 07:09:02 -0500

    crud in member controller

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,9 +1,34 @@
 class MembersController < ApplicationController
 
   def index
-    @members = Member.all
-    render json:@members
+    members = Member.all.sort_by{member.id}
+    render json:members
+  end
+
+  def create
+    member = Member.create!(member_params)
+    render json:member
+  end
+
+  def update
+    member = Member.find_by(params[:id])
+    member.update!(member_params)
+    render json: member
+  end
+
+  def destroy
+    member = Member.find_by(params[:id])
+    member.destroy!
+    render json: {}
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:first_name, :last_name, :member_number)
   end
 
 
 end
+
+
